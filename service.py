ebedac747f0517e9b5c064e769e81f5f987be9d3
Junshuai Zhang <juz1@aber.ac.uk>
2015-11-30 19:28:08 +0000

    Customers can post an order and index orders now

--- service.py
+++ service.py
@@ -1,20 +1,37 @@
 import web
 import json
 
-render = web.template.render('.')
 
-urls = ("/", "hello",
+class Item:
+    def __init__(self, title, price, quantity):
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+
+
+class Order:
+    def __init__(self, name, address, email, items):
+        self.name = name
+        self.address = address
+        self.email = email
+        self.items = items 
+
+
+orders = [Order('name', 'address', 'email', [Item('t1', .5, 1), Item('t2', .8, 2)]),
+          Order('n2', 'a2', 'e2', [Item('t3', .9, 3), Item('t4', 1., 4)])]
+
+urls = ("/", "index",
         "/?wines", "WineController",
         "/?orders", "OrderController")
 
 app = web.application(urls, globals())
 
-orders = []
 
-class hello:
+class index:
     def GET(self):
-        return '<a href="./wines">Get Details</a>' + \
-               '<a href="./orders">Orders</a>'
+        render = web.template.render('.')
+        return render.index(orders)
+
 
 class WineController:
     def GET(self):
@@ -24,11 +41,20 @@ class WineController:
 
 class OrderController:
     def GET(self):
-        return orders
+        pass
 
     def POST(self):
-        string = web.input().get('order')
-        return json.loads(string)
+        order_json = web.input().get('order')
+        order_d = json.loads(order_json)
+
+        items_json = web.input().get('items')
+        items_d = json.loads(items_json)
+
+        items = [Item(item_d[u'title'], item_d[u'price'], item_d[u'quantity']) for item_d in items_d]
+        orders.append(Order(order_d[u'name'], order_d[u'address'], order_d[u'email'], items))
+
+        web.ctx.status = '201 CREATED'
+
 
 if __name__ == "__main__":
     app.run()
